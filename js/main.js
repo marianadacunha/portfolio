ae9b4cf3f3a8248008bfd5f938b115efb874f790
Mariana Marques <marianadacunhamarques@gmail.com>
2020-11-05 16:58:35 -0300

    add screenshot to README.md

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
-const screenshot = document.getElementsByClassName("project-screenshot")
-
-function hover(screenshot) {
-    screenshot.setAttribute('src', `img/screenshot.png`);
+const LIST = {
+    screenshot1: {
+        hover: `img/screenshot1.png`,
+        default: `img/screenshot1_dark.png`
+    },
+    screenshot2: {
+        hover: `img/screenshot2.png`,
+        default: `img/screenshot2_dark.png`
+    },
+    screenshot3: {
+        hover: `img/screenshot3.png`,
+        default: `img/screenshot3_dark.png`
+    },
+    screenshot6: {
+        hover: `img/screenshot6.png`,
+        default: `img/screenshot6_dark.png`
+    }
 }
 
-function unhover(screenshot) {
-    screenshot.setAttribute('src', `img/screenshot${"_dark"}.png`);
+function hover(element) {
+    const item = element.getAttribute('type');
+    this.setAttribute(element, LIST[item]['hover'])
+}
+
+function unhover(element) {
+    const item = element.getAttribute('type');
+    this.setAttribute(element, LIST[item]['default'])
+}
+
+function setAttribute(element, value) {
+    return element.setAttribute('src', value);
 }
